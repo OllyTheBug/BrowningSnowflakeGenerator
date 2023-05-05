4e56d0737d2421a21c6d10828183a1e273eafaa6
OllyTheBug <mendelse@oregonstate.edu>
2023-05-04 19:50:23 -0700

    added color

--- browningSnowflakes_animated_pixelarray.js
+++ browningSnowflakes_animated_pixelarray.js
@@ -6,7 +6,7 @@
 /* ------------------------------- PARAMETERS ------------------------------- */
 let _frameRate = 60;
 let centerHorizontal;
-let leftness = 1.5;
+let leftness = 1;
 let rightness = 1;
 let density = 10;
 /* -------------------------------- INTERNAL -------------------------------- */
@@ -16,7 +16,10 @@ const cos45 = Math.cos(Math.PI / 4);
 let fallingParticles = [];
 let emitterPixel;
 let lockedIndexes = [];
+let currentColor = [0, 255, 255, 255]
+let indexColors = [];
 let img;
+let ctx;
 /* ------------------------------- //!SECTION ------------------------------- */
 
 /* -------------------------------------------------------------------------- */
@@ -26,17 +29,18 @@ let img;
 
 function setup() {
     let canvas = createCanvas(800, 800);
+    ctx = canvas.drawingContext;
+    ctx.shadowcolor = "rgba(0,0,0,0.5)";
     canvas.parent("sketch-holder");
     centerHorizontal = (width * 4) * (height / 2);
     emitterPixel = (width * 2);
     lockedIndexes = Array(width * height).fill(0);
+    indexColors = Array(width * height).fill(0);
 
     background(255);
     loadPixels();
-    //noStroke();
-    //fill(255);
 
-    pixelDensity(1);
+    pixelDensity(1.1);
     frameRate(_frameRate);
     imageMode(CENTER);
     img = createImage(width, height);
@@ -58,7 +62,7 @@ function draw() {
     }
     // Spawn particle
     if (fallingParticles.length < fallingLimit && frameCount % Math.floor(map(density, 1, 10, 60, 1)) === 0) {
-        fallingParticles.push({pos: indexToXY(emitterPixel), color: 255});
+        fallingParticles.push({ pos: indexToXY(emitterPixel), color: currentColor });
         // If new spawn is adjacent to landed particle, the snowflake is complete
         if (particleAdjacentToLanded(fallingParticles[fallingParticles.length - 1])) {
             noLoop();
@@ -69,12 +73,16 @@ function draw() {
     updateFallingParticles();
     landedToPixelArray();
     mirrorPixelArrayAcrossVertical();
-    mirrorPixelArrayAcrossHorizontal();
-    imposeRotatedPixelArray(getRotatedNinetyDeg());
+    // mirrorPixelArrayAcrossHorizontal();
+    // imposeRotatedPixelArray(getRotatedNinetyDeg());
     img.updatePixels();
-    image(img, 0, 0);
-    rotate(PI / 4);
-    image(img, 0, 0);
+    // image(img, 0, 0);
+    // rotate(PI / 4);
+    // image(img, 0, 0);
+    for (let i = 0; i<6; i++){
+        rotate(PI/3);
+        image(img, 0, 0);
+    }
 }
 
 /* ------------------------------ // !SECTION ------------------------------- */
@@ -106,13 +114,13 @@ function particleAdjacentToLanded(particle) {
     let root = xyToIndex(particle.pos);
     //check the eight pixels surrounding the particleA
     let adjacents = [root - 4,
-        root + 4,
-        root - (width * 4),
-        root + (width * 4),
-        root - (width * 4) - 4,
-        root - (width * 4) + 4,
-        root + (width * 4) - 4,
-        root + (width * 4) + 4]
+    root + 4,
+    root - (width * 4),
+    root + (width * 4),
+    root - (width * 4) - 4,
+    root - (width * 4) + 4,
+    root + (width * 4) - 4,
+    root + (width * 4) + 4]
     for (let i = 0; i < adjacents.length; i++) {
         if (lockedIndexes[adjacents[i] / 4] === 1) {
             return true;
@@ -143,23 +151,18 @@ function fall(particle) {
 }
 
 function land(particle) {
-    lockedIndexes[xyToIndex(particle.pos) / 4] = 1;
+    var root = xyToIndex(particle.pos);
+    lockedIndexes[root / 4] = 1;
+    indexColors[root / 4] = particle.color;
+
 }
 
 function particleToPixelArray(particle) {
     let root = xyToIndex(particle.pos);
-    img.pixels[root] = particle.color;
-    img.pixels[root + 1] = particle.color;
-    img.pixels[root + 2] = particle.color;
-    img.pixels[root + 3] = 255;
-}
-
-function xyToPixelArray(pos) {
-    let root = xyToIndex(pos);
-    img.pixels[root] = 255;
-    img.pixels[root + 1] = 255;
-    img.pixels[root + 2] = 255;
-    img.pixels[root + 3] = 255;
+    img.pixels[root] = particle.color[0];
+    img.pixels[root + 1] = particle.color[1];
+    img.pixels[root + 2] = particle.color[2];
+    img.pixels[root + 3] = particle.color[3];
 }
 
 function updateFallingParticles() {
@@ -176,10 +179,10 @@ function updateFallingParticles() {
 function landedToPixelArray() {
     for (let i = 0; i < lockedIndexes.length; i++) {
         if (lockedIndexes[i] === 1) {
-            img.pixels[i * 4] = 255;
-            img.pixels[i * 4 + 1] = 255;
-            img.pixels[i * 4 + 2] = 255;
-            img.pixels[i * 4 + 3] = 255;
+            img.pixels[i * 4] = indexColors[i][0];
+            img.pixels[i * 4 + 1] = indexColors[i][1];
+            img.pixels[i * 4 + 2] = indexColors[i][2];
+            img.pixels[i * 4 + 3] = indexColors[i][3];
         }
     }
 }
@@ -197,34 +200,21 @@ function addFallingParticlesToPixelArray() {
 
 function imposeRotatedPixelArray(rotatedPixels) {
     // OR the rotated pixels with the current pixels
-    for (let i = 0; i < pixels.length; i++) {
-        img.pixels[i] = (img.pixels[i] || rotatedPixels[i]) ? 255 : 0;
-    }
-}
-
-function getRotatedFourtyFiveDeg() {
-    let rotatedPixels = Array(pixels.length).fill(0);
-    for (let i = 0; i < pixels.length; i += 4) {
-        // if pixel (x,y) is white, make pixel (x+y,x-y) white
-        if (img.pixels[i] === 255) {
-            let pos = indexToXY(i);
-            let newPos = [pos[0] * cos45 - pos[1] * sin45, pos[0] * sin45 + pos[1] * cos45];
-            let newIndex = xyToIndex(newPos);
-            rotatedimg.pixels[newIndex] = 255;
-            rotatedimg.pixels[newIndex + 1] = 255;
-            rotatedimg.pixels[newIndex + 2] = 255;
-            rotatedimg.pixels[newIndex + 3] = 255;
+    for (let i = 0; i < img.pixels.length; i += 4) {
+        if (rotatedPixels[i + 3] === 255) {
+            img.pixels[i] = 255;
+            img.pixels[i + 1] = 255;
+            img.pixels[i + 2] = 255;
+            img.pixels[i + 3] = 255;
         }
     }
-    return rotatedPixels;
-
 }
 
 function getRotatedNinetyDeg() {
     let rotatedPixels = Array(pixels.length).fill(0);
     for (let i = 0; i < pixels.length; i += 4) {
 
-        if (img.pixels[i] === 255) {
+        if (img.pixels[i + 3] === 255) {
             let pos = indexToXY(i);
             let newPos = [-pos[1], pos[0]];
             let newIndex = xyToIndex(newPos);
@@ -241,14 +231,14 @@ function getRotatedNinetyDeg() {
 function mirrorPixelArrayAcrossVertical() {
     for (let i = 0; i < pixels.length; i += 4) {
         // if pixel (x,y) is white, make pixel (-y,x) white
-        if (img.pixels[i] === 255) {
+        if (img.pixels[i + 3] === 255) {
             let pos = indexToXY(i);
             let newPos = [width - pos[0], pos[1]];
             let newIndex = xyToIndex(newPos);
-            img.pixels[newIndex] = 255;
-            img.pixels[newIndex + 1] = 255;
-            img.pixels[newIndex + 2] = 255;
-            img.pixels[newIndex + 3] = 255;
+            img.pixels[newIndex] = img.pixels[i];
+            img.pixels[newIndex + 1] = img.pixels[i + 1];
+            img.pixels[newIndex + 2] = img.pixels[i + 2];
+            img.pixels[newIndex + 3] = img.pixels[i + 3];
         }
     }
 }
@@ -256,7 +246,7 @@ function mirrorPixelArrayAcrossVertical() {
 function mirrorPixelArrayAcrossHorizontal() {
     for (let i = 0; i < pixels.length; i += 4) {
         // if pixel (x,y) is white, make pixel (-y,x) white
-        if (img.pixels[i] === 255) {
+        if (img.pixels[i + 3] === 255) {
             let pos = indexToXY(i);
             let newPos = [pos[0], height - pos[1]];
             let newIndex = xyToIndex(newPos);
@@ -290,6 +280,10 @@ $("#framerate").on("input", function () {
     frameRate(this.value);
     $("#framern").text(this.value);
 });
+//When color slider changes, update currentColor
+$("#color-picker").on("input", function () {
+    currentColor = hexToRgb(this.value);
+});
 //When clear button is clicked, clear the canvas, reset the lockedIndexes, and reset the fallingParticles
 $("#clearButton").on("click", function () {
     clear();
@@ -306,3 +300,17 @@ $("#stopButton").on("click", function () {
 $("#playButton").on("click", function () {
     loop();
 });
+
+//Helper functions
+//Convert hex color to rgb array
+function hexToRgb(hex) {
+    if (hex.length === 4) {
+        hex = hex + hex[1] + hex[2] + hex[3];
+
+    }
+    let r = parseInt(hex.substring(1, 3), 16);
+    let g = parseInt(hex.substring(3, 5), 16);
+    let b = parseInt(hex.substring(5, 7), 16);
+    let a = 255;
+    return [r, g, b, a];
+}
